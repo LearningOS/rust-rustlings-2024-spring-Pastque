b1877356f1f3fe81b2931f67d1fd3810ba15bc58
Pastque <2844693943@qq.com>
2024-04-28 14:55:45 +0800

    four

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,15 +6,18 @@
 // For these exercises the code will fail to compile when there are clippy
 // warnings check clippy's suggestions from the output to solve the exercise.
 //
+// Clippy 工具是一组用于分析代码的 lint 程序，以便您可以捕获常见错误并改进Rust代码。
+// 对于这些练习，当有clippy警告时，代码将无法编译。请从输出中检查clippy的建议来解决练习。
+//
 // Execute `rustlings hint clippy1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::f32;
 
 fn main() {
-    let pi = 3.14f32;
+    let pi = f32::consts::PI;
     let radius = 5.00f32;
 
     let area = pi * f32::powi(radius, 2);
